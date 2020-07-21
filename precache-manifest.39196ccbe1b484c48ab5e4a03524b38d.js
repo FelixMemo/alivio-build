ecb4284ca24bca5320445e583c526f214a19f028
FelixMemo <68380241+FelixMemo@users.noreply.github.com>
2020-07-21 16:47:28 +0300

    Add files via upload

---
+++ precache-manifest.39196ccbe1b484c48ab5e4a03524b38d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e543ace68268d42a280630edc0072cf",
+    "url": "/alivio/index.html"
+  },
+  {
+    "revision": "8cf9a83d7d5b8ff6b6a1",
+    "url": "/alivio/static/css/main.82a0eb54.chunk.css"
+  },
+  {
+    "revision": "bcc93c3ffca320f73b77",
+    "url": "/alivio/static/js/2.8cf82ef1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/alivio/static/js/2.8cf82ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf9a83d7d5b8ff6b6a1",
+    "url": "/alivio/static/js/main.4461f8a8.chunk.js"
+  },
+  {
+    "revision": "6e4d74d9a91335193e79",
+    "url": "/alivio/static/js/runtime-main.9eff76df.js"
+  }
+]);
